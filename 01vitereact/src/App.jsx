fa4fe8bbc6b876d851872cf73653ed74688e5c5a
Romus Shrestha <sromus498@gmail.com>
2024-11-23 12:37:05 +0545

    creating react app and custom react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Fisrt from './first'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Fisrt />
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/First.jsx
@@ -0,0 +1,6 @@
+function Fisrt(){
+    return(
+        <h1>hello world this is first app</h1>
+    )
+}
+export default Fisrt;
